038f89423424fade7a7b6940db30cd4d8c7901c2
Brett Castellanos <brett.castellanos@gmail.com>
2021-01-03 10:45:49 -0800

    Update dz60 map

--- keyboards/dz60/keymaps/brett-castellanos/keymap.c
+++ keyboards/dz60/keymaps/brett-castellanos/keymap.c
@@ -7,10 +7,17 @@
 #define _RIGHT 3
 
 // Custom Keycodes
+#define CMD_Z LGUI(KC_Z)
+#define CMD_X LGUI(KC_X)
+#define CMD_C LGUI(KC_C)
+#define CMD_V LGUI(KC_V)
 #define GUI_CTL TD(TD_GUI_CTL)
 #define F_LEFT TD(TD_F_LEFT)
 #define J_RGHT TD(TD_J_RIGHT)
 #define ESC_HYP TD(TD_ESC_HYP)
+#define ESC_LFT LT(_LEFT, KC_ESC)
+#define ENT_RGT LT(_RIGHT, KC_ENT)
+#define FN MO(_FN)
 
 // Tap Dance declarations
 enum td_keycodes {
@@ -53,9 +60,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_MAC] = LAYOUT_all(
         KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC, KC_BSPC,
         KC_TAB,           KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,
-        ESC_HYP,          KC_A,    KC_S,    KC_D,    F_LEFT,  KC_G,    KC_H,    J_RGHT,  KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_ENT,
+        ESC_LFT,          KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, ENT_RGT,
         KC_LSFT, KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, KC_UP,   KC_DEL,
-        KC_LCTL, KC_LALT, KC_LGUI,                   KC_SPC,  KC_SPC,  KC_SPC,                    GUI_CTL, MO(_FN), KC_LEFT, KC_DOWN, KC_RIGHT
+        KC_LCTL, KC_LALT, KC_LGUI,                   KC_SPC,  KC_SPC,  KC_SPC,                    GUI_CTL, FN,      KC_LEFT, KC_DOWN, KC_RGHT
     ),
 
     [_FN] = LAYOUT_all(
@@ -67,18 +74,18 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
 
     [_LEFT] = LAYOUT_all(
-	    RESET,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,  KC_DEL,
-        _______,          RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD, RGB_VAI, RGB_VAD, _______, _______, _______, _______, _______,
-        _______,          _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+	    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+        _______,          _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+        _______,          _______, _______, _______, _______, _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______, _______, _______,
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_VOLU, _______,
         _______, _______, _______,                   _______, _______, _______,                   _______, _______, _______, KC_VOLD, _______
     ),
 
     [_RIGHT] = LAYOUT_all(
-	    RESET,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,  KC_DEL,
-        _______,          RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD, RGB_VAI, RGB_VAD, _______, _______, _______, _______, _______,
+	    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+        _______,          _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
         _______,          _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
-        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+        _______, _______, CMD_Z,   CMD_X,   CMD_C,   CMD_V,   _______, _______, _______, _______, _______, _______, _______, _______, _______,
         _______, _______, _______,                   _______, _______, _______,                   _______, _______, _______, _______, _______
     )
 };
